8909b965a2edfab2b9fc94faafce4816b8a4c3a4
Girisha97 <girishavanaparthi02@gmail.com>
2024-08-26 16:48:28 -0500

    Auto push

--- 02-ec2/provider.tf
+++ 02-ec2/provider.tf
@@ -1,1 +1,12 @@
-provider "aws" {}
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "2.36.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
