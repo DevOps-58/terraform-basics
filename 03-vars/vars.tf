205dd7701584ec6939411457b3ebdc3c3046979c
Girisha97 <girishavanaparthi02@gmail.com>
2024-08-28 16:12:29 -0500

    Auto push

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -39,3 +39,19 @@ output "test_op_works" {
     value = "current topic is ${var.test[1]} and this supports more than ${var.test[0]} cloud providers"
   
 }
+
+# Map Variable
+
+variable "m" {
+    default = {
+        name = "Girisha"
+        content = "Devops"
+        salary = 13000
+    }
+  
+}
+
+output "m_op" {
+    value = "${var.m[name]} is a ${var.m[content]} engineer and her salary is ${var.m[salary]}"
+  
+}
