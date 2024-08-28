87d940c83fd2fe51f861e780a493445f623a336d
Girisha97 <girishavanaparthi02@gmail.com>
2024-08-28 16:15:31 -0500

    Auto push

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -51,7 +51,7 @@ variable "m" {
   
 }
 
-output "m_op_words" {
-    value = "${var.m[name]} is a ${var.m[content]} engineer and her salary is ${var.m[salary]}"
+output "m_op" {
+    value = "${var.m["name"]} is a ${var.m["content"]} engineer and her salary is ${var.m["salary"]}"
   
 }
