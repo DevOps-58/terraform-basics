d0f8d3a5257b6d214bd261ff0a93a30307d07098
Girisha97 <girishavanaparthi02@gmail.com>
2024-08-28 20:04:53 -0500

    Auto push

--- expense-v3/ec2.tf
+++ expense-v3/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "main" {
   for_each               = var.components
   ami                    = "ami-0fcc78c828f981df2"
   instance_type          = each.value["instance_type"]
-  vpc_security_group_ids = ["sg-08c9eb09595f5de07"]
+  vpc_security_group_ids = ["sg-08e172f087724018a"]
 
   tags = {
     Name = each.key
