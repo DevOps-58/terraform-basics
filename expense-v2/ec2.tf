254d75425f4c323033d56d795456cefe72e0f11e
Girisha97 <girishavanaparthi02@gmail.com>
2024-08-28 19:30:07 -0500

    Auto push

---
+++ expense-v2/ec2.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "main" {
+  count                  = length(var.components)
+  ami                    = "ami-0fcc78c828f981df2"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-08e172f087724018a"]
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+variable "components" {
+  default = ["frontend" , "backend" , "mysql"]
+  
+}
+
