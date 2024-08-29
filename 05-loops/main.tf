3df0ec467b847776975facbe7f6f7823fc30c2a1
Girisha97 <girishavanaparthi02@gmail.com>
2024-08-28 19:17:14 -0500

    Auto push

--- 05-loops/main.tf
+++ 05-loops/main.tf
@@ -2,3 +2,13 @@ resource "null_resource" "main" {
     count = 3
   
 }
+
+resource "null_resource" "cars" {
+    count = length(var.cars)
+  
+}
+
+variable "cars" {
+    default = ["kia", "toyota", "benz"]
+  
+}
