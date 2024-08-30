35c7047fe5da9a93aef5f85817a45c47cdc0854a
Girisha97 <girishavanaparthi02@gmail.com>
2024-08-30 16:10:18 -0500

    Auto push

--- 08-modules/main.tf
+++ 08-modules/main.tf
@@ -1,10 +1,17 @@
+# Calling a backend module which is locally available 
+# Root Module
+
+# An example to demonstrate how to send the information from root module to the backend module while calling.
 module "test" {
-    source = "./sample"
-    colors = var.colors
-  
+  source = "./sample"
+  colors = var.colors # This is how we pass the info to the backend module from the root module ; step-3
 }
 
-variable "colors" {
-    default = ["blue" , "pink"]
-  
+
+variable "colors" {} # step-2
+
+### Whatever the variables that you're defining here, should also be defined in the backend as an empty variable
+
+output "dev" {
+  value = module.test.msg # ### Passing info from child-module to root - mdoule ; step-2
 }
