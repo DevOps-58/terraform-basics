1dca6a3b0e8c9f7986027dfc4907f64077642a67
Girisha97 <girishavanaparthi02@gmail.com>
2024-09-03 12:07:19 -0500

    Auto push

--- expense-v5/vars.tf
+++ expense-v5/vars.tf
@@ -1,23 +1,17 @@
 variable "components" {
   default = {
-    frontend = {
-      instance_type = "t3.small" 
+    mysql = {
+      instance_type = "t3.small"
     }
     backend = {
-      instance_type = "t3.micro" 
+      instance_type = "t3.micro" # each.value
     }
-    mysql = {
+    frontend = { # each.key
       instance_type = "t3.small"
     }
   }
 }
 
-variable "ami" {
-    default = "ami-0fcc78c828f981df2"
-  
-}
-
-variable  "vpc_security_group_ids" {
-    default = ["sg-08e172f087724018a"]
-  
+variable "pwd" {
+  default = "ExpenseApp@1"
 }
